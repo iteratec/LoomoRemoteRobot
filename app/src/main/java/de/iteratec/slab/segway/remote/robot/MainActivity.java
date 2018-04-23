83a8921e3aede6898fdc62711691d85d73a53255
Sigrun Eis <sei@iteratec.de>
2018-04-23 15:25:17 +0200

    SLAB-1092 Android STT recognition MainActivity

--- app/src/main/java/de/iteratec/slab/segway/remote/robot/MainActivity.java
+++ app/src/main/java/de/iteratec/slab/segway/remote/robot/MainActivity.java
@@ -1,12 +1,39 @@
 package de.iteratec.slab.segway.remote.robot;
 
 import android.content.Context;
+import android.media.MediaRecorder;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.*;
+import android.widget.Toast;
+import android.speech.*;
+
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.common.primitives.Bytes;
+import com.google.protobuf.ByteString;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.ArrayList;
 
 import de.iteratec.slab.segway.remote.robot.service.LoomoBaseService;
 import de.iteratec.slab.segway.remote.robot.service.LoomoConnectivityService;
@@ -29,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
     private LoomoEmojiService loomoEmojiService;
     private LoomoConnectivityService loomoConnectionService;
 
+    private Button recordButton;
+    private MediaRecorder mRecorder = null;
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +68,73 @@ public class MainActivity extends AppCompatActivity {
 
         statusText = (TextView) findViewById(R.id.status_text);
 
+        recordButton = (Button) findViewById(R.id.button);
+        recordButton.setOnClickListener(recordListener);
+
         // Hides the status bar
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         getSupportActionBar().hide();
 
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(getExternalCacheDir().getAbsolutePath() + "/test.3gpp");
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+
         initStatus();
         initServices();
     }
+
+    private Boolean recording = true;
+
+    public View.OnClickListener recordListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            promptSpeechInput();
+        }
+    };
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                //RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),"empty",
+                    Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "here", a);
+
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Toast.makeText(getApplicationContext(),(result.get(0)),
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+
+        }
+    }
+
 
     private void initStatus() {
         String ip = getDeviceIp();
@@ -86,5 +175,6 @@ public class MainActivity extends AppCompatActivity {
         this.loomoVisionService.disconnect();
         this.loomoHeadService.disconnect();
         this.loomoRecognitionService.disconnect();
+
     }
 }
